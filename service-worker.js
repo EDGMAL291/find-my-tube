dc46e7524064272ad3d56b7bb95bf49e0fda2ad2
Edgar Malesa <malesaedgar@gmail.com>
2026-03-16 23:29:22 +0200

    Fix mobile PWA cache updates

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = "find-my-tube-v120";
+const CACHE_NAME = "find-my-tube-v121";
 const CORE_ASSETS = [
   "./",
   "./index.html",
-  "./manifest.webmanifest",
-  "./assets/css/style.css?v=20260316a",
-  "./assets/js/script.js?v=20260316a",
-  "./assets/data/data.js?v=20260316a",
+  "./manifest.webmanifest?v=20260316b",
+  "./assets/css/style.css?v=20260316b",
+  "./assets/js/script.js?v=20260316b",
+  "./assets/data/data.js?v=20260316b",
   "./favicon.svg",
   "./favicon-16.png",
   "./favicon-32.png",
   "./assets/images/lab-bg.svg",
   "./assets/icons/favicon-16.png",
   "./assets/icons/favicon-32.png",
-  "./assets/icons/icon-192.png",
-  "./assets/icons/icon-512.png"
+  "./assets/icons/icon-192.png?v=20260316b",
+  "./assets/icons/icon-512.png?v=20260316b"
 ];
 
 self.addEventListener("install", (event) => {
@@ -51,9 +51,32 @@ self.addEventListener("fetch", (event) => {
     return;
   }
 
-  if (event.request.mode === "navigate") {
+  const isNavigationRequest = event.request.mode === "navigate";
+  const isVersionedAsset = requestUrl.searchParams.has("v");
+  const isFreshnessSensitiveRequest =
+    isNavigationRequest
+    || requestUrl.pathname.endsWith("/index.html")
+    || requestUrl.pathname.endsWith("/manifest.webmanifest")
+    || isVersionedAsset;
+
+  if (isFreshnessSensitiveRequest) {
     event.respondWith(
-      fetch(event.request).catch(() => caches.match("./index.html"))
+      fetch(event.request)
+        .then((response) => {
+          if (!response || response.status !== 200 || response.type !== "basic") {
+            return response;
+          }
+
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(async () => {
+          const cachedResponse = await caches.match(event.request);
+          if (cachedResponse) return cachedResponse;
+          if (isNavigationRequest) return caches.match("./index.html");
+          return undefined;
+        })
     );
     return;
   }
