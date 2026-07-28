c4028c7201c4784cc604ec4d3c9e468f330dff83
Edgar Malesa <malesaedgar@gmail.com>
2026-07-28 14:46:53 +0200

    Lower homepage carousel content

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "find-my-tube-v226";
+const CACHE_NAME = "find-my-tube-v227";
 const isLocalPreview = () => (
   self.location.hostname === "127.0.0.1"
   || self.location.hostname === "localhost"
@@ -18,7 +18,7 @@ const CORE_ASSETS = [
   "./sitemap.xml",
   "./manifest.webmanifest?v=20260316b",
   "./assets/css/style.css?v=20260728h",
-  "./assets/css/modern.css?v=20260728ag",
+  "./assets/css/modern.css?v=20260728ah",
   "./assets/js/script.js?v=20260728n",
   "./assets/js/find-my-test.js?v=20260728a",
   "./assets/data/data.js?v=20260505a",
