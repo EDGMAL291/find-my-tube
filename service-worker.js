75890a828b78f8078dfb47ee7da1c729f429d2b1
Edgar Malesa <malesaedgar@gmail.com>
2026-07-28 14:00:26 +0200

    Reduce display typography across the site

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "find-my-tube-v218";
+const CACHE_NAME = "find-my-tube-v219";
 const isLocalPreview = () => (
   self.location.hostname === "127.0.0.1"
   || self.location.hostname === "localhost"
@@ -17,7 +17,7 @@ const CORE_ASSETS = [
   "./sitemap.xml",
   "./manifest.webmanifest?v=20260316b",
   "./assets/css/style.css?v=20260728h",
-  "./assets/css/modern.css?v=20260728y",
+  "./assets/css/modern.css?v=20260728z",
   "./assets/js/script.js?v=20260728l",
   "./assets/js/find-my-test.js?v=20260728a",
   "./assets/data/data.js?v=20260505a",
