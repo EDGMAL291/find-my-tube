a3fcfb5b6b13efaf38f185514a25c7b14cd5502d
Edgar Malesa <malesaedgar@gmail.com>
2026-07-28 14:33:30 +0200

    Unify homepage and Find My Tube branding

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "find-my-tube-v224";
+const CACHE_NAME = "find-my-tube-v225";
 const isLocalPreview = () => (
   self.location.hostname === "127.0.0.1"
   || self.location.hostname === "localhost"
@@ -17,7 +17,7 @@ const CORE_ASSETS = [
   "./sitemap.xml",
   "./manifest.webmanifest?v=20260316b",
   "./assets/css/style.css?v=20260728h",
-  "./assets/css/modern.css?v=20260728ae",
+  "./assets/css/modern.css?v=20260728af",
   "./assets/js/script.js?v=20260728n",
   "./assets/js/find-my-test.js?v=20260728a",
   "./assets/data/data.js?v=20260505a",
