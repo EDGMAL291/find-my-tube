2dec3f09697e5b663b6dc3e7959567fb58145407
Edgar Malesa <malesaedgar@gmail.com>
2026-07-28 11:07:05 +0200

    Refine homepage and Suggest Tests design

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "find-my-tube-v205";
+const CACHE_NAME = "find-my-tube-v207";
 const isLocalPreview = () => (
   self.location.hostname === "127.0.0.1"
   || self.location.hostname === "localhost"
@@ -17,7 +17,7 @@ const CORE_ASSETS = [
   "./sitemap.xml",
   "./manifest.webmanifest?v=20260316b",
   "./assets/css/style.css?v=20260728h",
-  "./assets/css/modern.css?v=20260728m",
+  "./assets/css/modern.css?v=20260728o",
   "./assets/js/script.js?v=20260728i",
   "./assets/js/find-my-test.js?v=20260603b",
   "./assets/data/data.js?v=20260505a",
