20b747cd3d3d03aa456d196a74dab1b5506657e7
Edgar Malesa <malesaedgar@gmail.com>
2026-07-29 17:56:17 +0200

    Align Find My Test hero left on desktop

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "find-my-tube-v230";
+const CACHE_NAME = "find-my-tube-v231";
 const isLocalPreview = () => (
   self.location.hostname === "127.0.0.1"
   || self.location.hostname === "localhost"
@@ -17,7 +17,7 @@ const CORE_ASSETS = [
   "./robots.txt",
   "./sitemap.xml",
   "./manifest.webmanifest?v=20260316b",
-  "./assets/css/style.css?v=20260728h",
+  "./assets/css/style.css?v=20260729a",
   "./assets/css/modern.css?v=20260729a",
   "./assets/js/script.js?v=20260728o",
   "./assets/js/find-my-test.js?v=20260728b",
